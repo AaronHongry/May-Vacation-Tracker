e6caae9afa32200bb8fffe64d0e4c51d99896596
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2024-04-30 21:06:08 -0400

    added delete swipe and opacity

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -9,14 +9,15 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ExpenseProps } from "@/types";
 import AddExpense from "./addExpense";
 import { db } from "@/firebase/config";
 import { collection, getDocs } from 'firebase/firestore';
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, PanInfo, useMotionValue, useTransform} from "framer-motion";
 import EditExpense from "./editExpense";
-
+import { Trash2 } from "lucide-react";
+ 
 interface CategoryProps {
   name: string;
   nameSingle: string;
@@ -38,7 +39,7 @@ async function fetchCollectionData(collectionName: string): Promise<ExpenseProps
 
 function Expense({id, name, amount, people, date}: ExpenseProps) {
   return (
-    <motion.div initial={{ opacity: 0, x: "-30%" }} animate={{ opacity: 1, x: "0%", transition: { delay: 0.25 } }} transition={{ duration: 1, ease: [0, 1, 0, 1] }} whileTap={{ scale: 1.1, backgroundColor: "rgb(51 65 85)"}} className="grid grid-cols-5 gap-4 py-2 px-4 w-full items-center bg-slate-800 rounded-lg ">
+    <motion.div initial={{ opacity: 0, x: "-30%" }} animate={{ opacity: 1, x: "0%", transition: { delay: 0.25 } }} transition={{ duration: 1, ease: [0, 1, 0, 1] }} className="grid grid-cols-5 gap-4 py-2 px-4 w-full items-center bg-slate-800 rounded-lg ">
       <div className="col-span-3">
         <p className="text-2xl font-bold">{name}</p>
         {people.map((person, index) => (
@@ -57,10 +58,18 @@ function Expense({id, name, amount, people, date}: ExpenseProps) {
 const Category: React.FC<CategoryProps> = ({name, nameSingle, description, collection}) => {
   const [expenses, setExpenses] = useState<ExpenseProps[]>();
   const [expenseOpen, setExpenseOpen] = useState<string | null>(null);
+  
+  const [tapPos, setTapPos] = useState(0.0);
+  const [doubleTap, setDoubleTap] = useState("");
+
+  const x = useMotionValue(0);
+  const opacity = useTransform(x, [-40, 0], [0.4, 0]);
+  const expensesRef = useRef(new Map);
 
   const fetchExpenses = async () => {
     try {
       const fetchedData = await fetchCollectionData(collection);
+      console.log("fetchCat");
       setExpenses(fetchedData);
     } catch (error) {
       console.error("Failed to fetch expenses:", error);
@@ -88,6 +97,25 @@ const Category: React.FC<CategoryProps> = ({name, nameSingle, description, colle
     }, 600);
   };
 
+  const handleExpenseEdit = (id: string) => {
+    setDoubleTap(i => {
+      if (i == id) {
+        setExpenseOpen(id);
+        setDoubleTap("");
+      }
+      return id;
+    });
+    setTimeout(() => setDoubleTap(""), 1000);
+  }
+
+  const handleDelete = (event: MouseEvent | TouchEvent, info: PanInfo) => {
+    const currentPoint = Math.abs(info.point.x - tapPos) * 0.3076;
+    console.log(currentPoint)
+    if (currentPoint < -130) {
+      console.log(currentPoint);
+    }
+  };
+
   return (
     <AnimatePresence>
       <Card className="bg-slate-950 border-slate-800 rounded-2xl border-2">
@@ -101,7 +129,13 @@ const Category: React.FC<CategoryProps> = ({name, nameSingle, description, colle
               {expenses?.length == 0 && <p className="text-gray-400 text-sm text-center">There are no expenses added!</p>}
               {expenses?.map(expense => (
                 <div key={expense.id}>
-                  <motion.div drag="x" dragConstraints={{ left: 0, right: 0 }} dragElastic={{ left: 0.2, right: 0 }} onDragEnd={(e, i) => console.log(i.point.x)} onClick={() => {setExpenseOpen(expense.id)}}><Expense id={expense.id} name={expense.name} amount={expense.amount} people={expense.people} date={expense.date}/></motion.div>
+                  <motion.div className="relative overflow-hidden" drag="x" dragConstraints={{ left: 0, right: 0 }} dragElastic={{ left: 0.2, right: 0 }} onDragStart={(e, i) => {setTapPos(i.point.x)}} whileTap={{ scale: 1.1, backgroundColor: "rgb(51 65 85)"}} onDrag={handleDelete} onDragEnd={()=>{}} onClick={() => {handleExpenseEdit(expense.id)}} style={{x}}>
+                    <motion.div id="delete" className={`absolute rounded-xl z-40 w-full h-full bg-red-500 flex justify-center items-center`} style={{opacity: opacity}}>
+                      <Trash2 className="absolute rounded-xl size-14 text-red-500 z-50 "/>
+                      <div className="absolute w-full h-full z-30 rounded-xl bg-red-400"></div>
+                    </motion.div>
+                    <Expense id={expense.id} name={expense.name} amount={expense.amount} people={expense.people} date={expense.date}/>
+                  </motion.div>
                   {expenseOpen == expense.id && <EditExpense id={expense.id} name={name} nameSingle={nameSingle} onExpenseEdit={handleExpensesChanged} isOpen={expenseOpen == expense.id} onClose={handleExpenseClose} currentName={expense.name} currentAmount={expense.amount} currentPeople={expense.people}/>}
                 </div> 
               ))}
